3dae0b794fa07048cc3f6a5b385af1906482c589
David McArthur <david.mcarthur72@outlook.com>
2019-11-06 09:39:29 -0800

    init commit

--- BaseClassDerivedClassMod6/Program.cs
+++ BaseClassDerivedClassMod6/Program.cs
@@ -6,7 +6,36 @@ namespace BaseClassDerivedClassMod6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fruit raspberry = new Fruit();
+            raspberry.Name = "Raspberry";
+            raspberry.Edible = true;
+            raspberry.PlantType = "Vine";
         }
+    }
+
+    public abstract class Plant
+    {
+        public string Name { get; set; }
+        public string PlantType { get; set; }
+        public bool Edible { get; set; }
+        
+
+    }
+
+    public class Veggies : Plant   
+    {
+        // gord, tubor, leaf, etc.
+        public string VeggiType { get; set; }
+    }
+
+    class Fruit : Plant
+    {
+        // berry, stone fruit, tree fruit, etc
+        public string FruitType { get; set; }
+    }
+
+    class Fungi : Plant
+    {
+
     }
 }
